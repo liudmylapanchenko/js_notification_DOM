fc6a858ed85fefb0ae76a9f7118395cdd4e1146c
Liudmyla Panchenko <liudmyla.panchenko29@gmail.com>
2025-06-04 17:28:35 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    notification.classList.add("notification", type);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    const notificationTitle = document.createElement("h2");
+    notificationTitle.classList.add("title");
+    notificationTitle.textContent = title;
+    const notificationText = document.createElement("p");
+    notificationText.textContent = description;
+    notification.appendChild(notificationTitle);
+    notification.appendChild(notificationText);
+    document.body.appendChild(notification);
+    setTimeout(()=>{
+        notification.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
